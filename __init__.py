8c4d7cf52fe7fb1c94aaac6d2994ebd2e4eb29cb
EvATive7 <evative7@gmail.com>
2024-06-02 05:25:41 +0800

    chore

--- __init__.py
+++ __init__.py
@@ -6,10 +6,11 @@ import image
 class TestPusher(Pusher):
     async def push(self, content: Struct, to: str) -> PushResult:
         try:
-            print(f'This article will be pushed to {to}: ')
-            print('==================')
-            print(content.asmarkdown())
-            print('==================')
+            # print(f'This article will be pushed to {to}: ')
+            # print('==================')
+            # print(content.asmarkdown())
+            # print('==================')
+            self.logger.info('Pushing...')
             ok = True
             _e = None
         except Exception as e:
